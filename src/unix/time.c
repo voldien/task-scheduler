f1663343507243a82e845e847c97bbcdf9d51261
Voldie <voldiekami@gmail.com>
2019-01-08 07:36:52 +0100

    Changed from double to long int.

--- src/unix/time.c
+++ src/unix/time.c
@@ -8,11 +8,12 @@
 long int schGetTime(void) {
 	struct timeval tSpec;
 	gettimeofday(&tSpec, NULL);
-	return (tSpec.tv_sec * 1e6L + tSpec.tv_usec) * 1000;
+	return (tSpec.tv_sec * 1000000 + tSpec.tv_usec) * 1000;
 }
 
 long int schTimeResolution(void) {
 	struct timespec spec;
 	clock_getres(CLOCK_MONOTONIC, &spec);
-	return (1E9L / spec.tv_nsec);
+	return (1000000000 / spec.tv_nsec);
+
 }
