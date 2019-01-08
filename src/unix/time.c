2a5932036b5cdce80109e1858c44e44ba73d4f96
Voldie <voldiekami@gmail.com>
2019-01-08 07:58:40 +0100

    Added additional time functionality.

--- src/unix/time.c
+++ src/unix/time.c
@@ -7,8 +7,13 @@
 
 long int schGetTime(void) {
 	struct timeval tSpec;
-	gettimeofday(&tSpec, NULL);
-	return (tSpec.tv_sec * 1000000 + tSpec.tv_usec) * 1000;
+	if(gettimeofday(&tSpec, NULL) == 0)
+		return (tSpec.tv_sec * 1000000 + tSpec.tv_usec) * 1000;
+	else{
+		struct timespec now;
+		clock_gettime(CLOCK_MONOTONIC_RAW, &now);
+		return now.tv_sec * 1000000000 + now.tv_nsec;
+	}
 }
 
 long int schTimeResolution(void) {
