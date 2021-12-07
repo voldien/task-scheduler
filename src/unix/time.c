96aed913fd84b8de5bbab8139e3daf3524f2165e
Valdemar Lindberg <voldiekami@gmail.com>
2021-12-07 07:26:10 +0100

    Added include for time

--- src/unix/time.c
+++ src/unix/time.c
@@ -1,13 +1,14 @@
 #include "internal/time.h"
 #include <bits/time.h>
 #include <sys/time.h>
+#include <time.h>
 #include <unistd.h>
 
 long int schGetTime(void) {
 	struct timeval tSpec;
-	if (gettimeofday(&tSpec, NULL) == 0)
+	if (gettimeofday(&tSpec, NULL) == 0) {
 		return (tSpec.tv_sec * 1000000 + tSpec.tv_usec) * 1000;
-	else {
+	} else {
 		struct timespec now;
 		clock_gettime(CLOCK_MONOTONIC_RAW, &now);
 		return now.tv_sec * 1000000000 + now.tv_nsec;
